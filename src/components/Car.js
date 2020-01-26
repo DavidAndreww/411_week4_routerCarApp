9d21cfc3a77ecf35f78bea64121559531268470f
David Bille <d_bille@yahoo.com>
2020-01-26 13:27:12 -0600

    cool beans

--- src/components/Car.js
+++ src/components/Car.js
@@ -9,7 +9,7 @@ const Car = props => {
   let car = cars[id];
   console.log(car);
   return (
-    <Container maxWidth="sm">
+    <Container fixed maxWidth="xsit ">
       <Paper elvation={3}>
         <h1>{car.Name}</h1>
         <Chip label={`id: ${car.id}`} />
